ed727e368d79be39ff87ac81cfa29d8b0cb2d939
Brendan McMahon <bernos@gmail.com>
2017-02-07 08:43:23 +1100

    First cut deploy service via cfn. Only web service updated so far

--- pkg/ecso/api/service_up.go
+++ pkg/ecso/api/service_up.go
@@ -19,20 +19,48 @@ func (api *api) ServiceUp(project *ecso.Project, env *ecso.Environment, service 
 		return err
 	}
 
+	// set env vars so that they are available when converting the docker
+	// compose file to a task definition
 	if err := api.setEnv(project, env, service); err != nil {
 		return err
 	}
 
-	if err := api.deployServiceStack(reg, project, env, service); err != nil {
+	// register task
+	taskDefinition, err := api.registerECSTaskDefinition(reg, project, env, service)
+
+	if err != nil {
 		return err
 	}
 
-	if err := api.deployECSService(reg, project, env, service); err != nil {
+	// deploy the service cfn stack
+	if err := api.deployServiceStack(reg, project, env, service, taskDefinition); err != nil {
 		return err
 	}
 
 	return nil
 }
+
+// func (api *api) _ServiceUp(project *ecso.Project, env *ecso.Environment, service *ecso.Service) error {
+// 	reg, err := api.cfg.GetAWSClientRegistry(env.Region)
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if err := api.setEnv(project, env, service); err != nil {
+// 		return err
+// 	}
+
+// 	if err := api._deployServiceStack(reg, project, env, service); err != nil {
+// 		return err
+// 	}
+
+// 	if err := api.deployECSService(reg, project, env, service); err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
 
 func (api *api) setEnv(project *ecso.Project, env *ecso.Environment, service *ecso.Service) error {
 	if err := util.AnyError(
@@ -52,7 +80,7 @@ func (api *api) setEnv(project *ecso.Project, env *ecso.Environment, service *ec
 	return nil
 }
 
-func (api *api) deployServiceStack(reg *ecso.AWSClientRegistry, project *ecso.Project, env *ecso.Environment, service *ecso.Service) error {
+func (api *api) deployServiceStack(reg *ecso.AWSClientRegistry, project *ecso.Project, env *ecso.Environment, service *ecso.Service, taskDefinition *ecs.TaskDefinition) error {
 	var (
 		cfg       = api.cfg
 		log       = cfg.Logger
@@ -63,7 +91,7 @@ func (api *api) deployServiceStack(reg *ecso.AWSClientRegistry, project *ecso.Pr
 		cfn       = reg.CloudFormationService(api.cfg.Logger.PrefixPrintf("  "))
 	)
 
-	params, err := getServiceStackParameters(cfn, project, env, service)
+	params, err := getServiceStackParameters(cfn, project, env, service, taskDefinition)
 
 	if err != nil {
 		return err
@@ -93,7 +121,48 @@ func (api *api) deployServiceStack(reg *ecso.AWSClientRegistry, project *ecso.Pr
 	return nil
 }
 
-func getServiceStackParameters(cfn services.CloudFormationService, project *ecso.Project, env *ecso.Environment, service *ecso.Service) (map[string]string, error) {
+// func (api *api) _deployServiceStack(reg *ecso.AWSClientRegistry, project *ecso.Project, env *ecso.Environment, service *ecso.Service) error {
+// 	var (
+// 		cfg       = api.cfg
+// 		log       = cfg.Logger
+// 		bucket    = env.CloudFormationBucket
+// 		stackName = service.GetCloudFormationStackName(env)
+// 		prefix    = service.GetCloudFormationBucketPrefix(env)
+// 		template  = service.GetCloudFormationTemplateFile()
+// 		cfn       = reg.CloudFormationService(api.cfg.Logger.PrefixPrintf("  "))
+// 	)
+
+// 	params, err := getServiceStackParameters(cfn, project, env, service)
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	tags := getServiceStackTags(project, env, service)
+
+// 	log.Infof("Deploying service cloudformation stack '%s'...", stackName)
+
+// 	result, err := cfn.PackageAndDeploy(
+// 		stackName,
+// 		template,
+// 		bucket,
+// 		prefix,
+// 		tags,
+// 		params,
+// 		false)
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if !result.DidRequireUpdating {
+// 		log.Printf("  No updates were required to Cloud Formation stack '%s'\n", result.StackID)
+// 	}
+
+// 	return nil
+// }
+
+func getServiceStackParameters(cfn services.CloudFormationService, project *ecso.Project, env *ecso.Environment, service *ecso.Service, taskDefinition *ecs.TaskDefinition) (map[string]string, error) {
 
 	outputs, err := cfn.GetStackOutputs(env.GetCloudFormationStackName())
 
@@ -101,17 +170,18 @@ func getServiceStackParameters(cfn services.CloudFormationService, project *ecso
 		return nil, err
 	}
 
-	var params map[string]string
+	params := map[string]string{
+		"Cluster":        outputs["Cluster"],
+		"DesiredCount":   fmt.Sprintf("%d", service.DesiredCount),
+		"TaskDefinition": *taskDefinition.TaskDefinitionArn,
+	}
 
-	if len(service.Route) == 0 {
-		params = make(map[string]string)
-	} else {
-		params = map[string]string{
-			"VPC":           outputs["VPC"],
-			"Listener":      outputs["Listener"],
-			"Path":          service.Route,
-			"RoutePriority": strconv.Itoa(service.RoutePriority),
-		}
+	if len(service.Route) > 0 {
+		params["VPC"] = outputs["VPC"]
+		params["Listener"] = outputs["Listener"]
+		params["Path"] = service.Route
+		params["Port"] = fmt.Sprintf("%d", service.Port)
+		params["RoutePriority"] = strconv.Itoa(service.RoutePriority)
 	}
 
 	for k, v := range service.Environments[env.Name].CloudFormationParameters {
@@ -134,147 +204,137 @@ func getServiceStackTags(project *ecso.Project, env *ecso.Environment, service *
 	return tags
 }
 
-func (api *api) deployECSService(reg *ecso.AWSClientRegistry, project *ecso.Project, env *ecso.Environment, service *ecso.Service) error {
-	var (
-		cfg = api.cfg
-		log = cfg.Logger
+// func (api *api) deployECSService(reg *ecso.AWSClientRegistry, project *ecso.Project, env *ecso.Environment, service *ecso.Service) error {
+// 	var (
+// 		cfg = api.cfg
+// 		log = cfg.Logger
 
-		cluster        = env.GetClusterName()
-		stackName      = service.GetCloudFormationStackName(env)
-		ecsServiceName = service.GetECSServiceName()
+// 		cluster        = env.GetClusterName()
+// 		stackName      = service.GetCloudFormationStackName(env)
+// 		ecsServiceName = service.GetECSServiceName()
 
-		ecsClient  = reg.ECSAPI()
-		cfnService = reg.CloudFormationService(log.PrefixPrintf("  "))
-		ecsService = reg.ECSService(log.PrefixPrintf("  "))
-	)
+// 		ecsClient  = reg.ECSAPI()
+// 		cfnService = reg.CloudFormationService(log.PrefixPrintf("  "))
+// 		ecsService = reg.ECSService(log.PrefixPrintf("  "))
+// 	)
 
-	taskDefinition, err := api.registerECSTaskDefinition(reg, project, env, service)
+// 	taskDefinition, err := api.registerECSTaskDefinition(reg, project, env, service)
 
-	if err != nil {
-		return err
-	}
+// 	if err != nil {
+// 		return err
+// 	}
 
-	serviceStackOutputs, err := cfnService.GetStackOutputs(stackName)
+// 	serviceStackOutputs, err := cfnService.GetStackOutputs(stackName)
 
-	if err != nil {
-		return err
-	}
+// 	if err != nil {
+// 		return err
+// 	}
 
-	services, err := ecsClient.DescribeServices(&ecs.DescribeServicesInput{
-		Services: []*string{
-			aws.String(ecsServiceName),
-		},
-		Cluster: aws.String(cluster),
-	})
+// 	services, err := ecsClient.DescribeServices(&ecs.DescribeServicesInput{
+// 		Services: []*string{
+// 			aws.String(ecsServiceName),
+// 		},
+// 		Cluster: aws.String(cluster),
+// 	})
 
-	if err != nil {
-		return err
-	}
+// 	if err != nil {
+// 		return err
+// 	}
 
-	log.Infof("Deploying ECS service '%s'", ecsServiceName)
+// 	log.Infof("Deploying ECS service '%s'", ecsServiceName)
 
-	isCreate := true
+// 	isCreate := true
 
-	for _, s := range services.Services {
-		if *s.Status != "INACTIVE" {
-			isCreate = false
-		}
-	}
+// 	for _, s := range services.Services {
+// 		if *s.Status != "INACTIVE" {
+// 			isCreate = false
+// 		}
+// 	}
 
-	if isCreate {
-		log.Printf("  Creating new ecs service...\n")
+// 	if isCreate {
+// 		log.Printf("  Creating new ecs service...\n")
 
-		input := &ecs.CreateServiceInput{
-			DesiredCount:   aws.Int64(int64(service.DesiredCount)),
-			ServiceName:    aws.String(ecsServiceName),
-			TaskDefinition: taskDefinition.TaskDefinitionArn,
-			Cluster:        aws.String(cluster),
-			DeploymentConfiguration: &ecs.DeploymentConfiguration{
-				MaximumPercent:        aws.Int64(200),
-				MinimumHealthyPercent: aws.Int64(100),
-			},
-		}
+// 		input := &ecs.CreateServiceInput{
+// 			DesiredCount:   aws.Int64(int64(service.DesiredCount)),
+// 			ServiceName:    aws.String(ecsServiceName),
+// 			TaskDefinition: taskDefinition.TaskDefinitionArn,
+// 			Cluster:        aws.String(cluster),
+// 			DeploymentConfiguration: &ecs.DeploymentConfiguration{
+// 				MaximumPercent:        aws.Int64(200),
+// 				MinimumHealthyPercent: aws.Int64(100),
+// 			},
+// 		}
 
-		if len(service.Route) > 0 {
-			input.LoadBalancers = []*ecs.LoadBalancer{
-				{
-					ContainerName:  aws.String("web"),
-					ContainerPort:  aws.Int64(int64(service.Port)),
-					TargetGroupArn: aws.String(serviceStackOutputs["TargetGroup"]),
-				},
-			}
+// 		if len(service.Route) > 0 {
+// 			input.LoadBalancers = []*ecs.LoadBalancer{
+// 				{
+// 					ContainerName:  aws.String("web"),
+// 					ContainerPort:  aws.Int64(int64(service.Port)),
+// 					TargetGroupArn: aws.String(serviceStackOutputs["TargetGroup"]),
+// 				},
+// 			}
 
-			input.Role = aws.String(serviceStackOutputs["ServiceRole"])
-		}
+// 			input.Role = aws.String(serviceStackOutputs["ServiceRole"])
+// 		}
 
-		_, err := ecsClient.CreateService(input)
+// 		_, err := ecsClient.CreateService(input)
 
-		if err != nil {
-			return err
-		}
+// 		if err != nil {
+// 			return err
+// 		}
 
-		log.Printf("  Create successful\n")
-	} else {
-		log.Printf("  Updating existing ecs service...\n")
+// 		log.Printf("  Create successful\n")
+// 	} else {
+// 		log.Printf("  Updating existing ecs service...\n")
 
-		_, err := ecsClient.UpdateService(&ecs.UpdateServiceInput{
-			DesiredCount:   aws.Int64(int64(service.DesiredCount)),
-			Service:        aws.String(ecsServiceName),
-			TaskDefinition: taskDefinition.TaskDefinitionArn,
-			Cluster:        aws.String(cluster),
-			DeploymentConfiguration: &ecs.DeploymentConfiguration{
-				MaximumPercent:        aws.Int64(200),
-				MinimumHealthyPercent: aws.Int64(100),
-			},
-		})
+// 		_, err := ecsClient.UpdateService(&ecs.UpdateServiceInput{
+// 			DesiredCount:   aws.Int64(int64(service.DesiredCount)),
+// 			Service:        aws.String(ecsServiceName),
+// 			TaskDefinition: taskDefinition.TaskDefinitionArn,
+// 			Cluster:        aws.String(cluster),
+// 			DeploymentConfiguration: &ecs.DeploymentConfiguration{
+// 				MaximumPercent:        aws.Int64(200),
+// 				MinimumHealthyPercent: aws.Int64(100),
+// 			},
+// 		})
 
-		if err != nil {
-			return err
-		}
+// 		if err != nil {
+// 			return err
+// 		}
 
-		log.Printf("  Update successful\n")
-	}
+// 		log.Printf("  Update successful\n")
+// 	}
 
-	log.Printf("\n")
-	log.Infof("Waiting for service to become stable...")
+// 	log.Printf("\n")
+// 	log.Infof("Waiting for service to become stable...")
 
-	cancel := ecsService.LogServiceEvents(ecsServiceName, env.GetClusterName(), func(e *ecs.ServiceEvent, err error) {
-		if err == nil && e != nil {
-			log.Printf("  %s %s\n", *e.CreatedAt, *e.Message)
-		}
-	})
+// 	cancel := ecsService.LogServiceEvents(ecsServiceName, env.GetClusterName(), func(e *ecs.ServiceEvent, err error) {
+// 		if err == nil && e != nil {
+// 			log.Printf("  %s %s\n", *e.CreatedAt, *e.Message)
+// 		}
+// 	})
 
-	defer cancel()
+// 	defer cancel()
 
-	if err := ecsClient.WaitUntilServicesStable(&ecs.DescribeServicesInput{
-		Services: []*string{
-			aws.String(ecsServiceName),
-		},
-		Cluster: aws.String(cluster),
-	}); err != nil {
-		return err
-	}
+// 	if err := ecsClient.WaitUntilServicesStable(&ecs.DescribeServicesInput{
+// 		Services: []*string{
+// 			aws.String(ecsServiceName),
+// 		},
+// 		Cluster: aws.String(cluster),
+// 	}); err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func (api *api) registerECSTaskDefinition(reg *ecso.AWSClientRegistry, project *ecso.Project, env *ecso.Environment, service *ecso.Service) (*ecs.TaskDefinition, error) {
 	var (
-		cfg = api.cfg
-		log = cfg.Logger
-
-		stackName = service.GetCloudFormationStackName(env)
+		cfg       = api.cfg
+		log       = cfg.Logger
 		taskName  = service.GetECSTaskDefinitionName(env)
-
-		ecsClient  = reg.ECSAPI()
-		cfnService = reg.CloudFormationService(log.PrefixPrintf("  "))
+		ecsClient = reg.ECSAPI()
 	)
-
-	serviceStackOutputs, err := cfnService.GetStackOutputs(stackName)
-
-	if err != nil {
-		return nil, err
-	}
 
 	// TODO: fully qualify the path to the service compose file
 	// taskDefinition, err := ConvertToTaskDefinition(taskName, service.ComposeFile)
@@ -295,10 +355,14 @@ func (api *api) registerECSTaskDefinition(reg *ecso.AWSClientRegistry, project *
 			LogDriver: aws.String(ecs.LogDriverAwslogs),
 			Options: map[string]*string{
 				"awslogs-region": aws.String(env.Region),
-				"awslogs-group":  aws.String(serviceStackOutputs["CloudWatchLogsGroup"]),
+				"awslogs-group":  aws.String(service.GetCloudWatchLogGroupName(env)),
 			},
 		})
 
+		// TODO probably don't automatically add service discovery env config
+		//      if people want to use service discover they can just add the
+		//      required env vars to the services docker-compose file. This is
+		//      less magic and more flexible
 		for _, p := range container.PortMappings {
 			container.Environment = append(container.Environment, &ecs.KeyValuePair{
 				Name:  aws.String(fmt.Sprintf("SERVICE_%d_NAME", *p.ContainerPort)),
--- pkg/ecso/templates/service.go
+++ pkg/ecso/templates/service.go
@@ -47,6 +47,142 @@ Parameters:
         Description: The VPC that the ECS cluster is deployed to
         Type: AWS::EC2::VPC::Id
 
+    Cluster:
+        Description: The name of the ECS cluster to deploy to
+        Type: String
+
+    DesiredCount:
+        Description: The number of instances of the service to run
+        Type: Number
+
+    Listener:
+        Description: The Application Load Balancer listener to register with
+        Type: String
+
+    Path:
+        Description: The path to register with the Application Load Balancer
+        Type: String
+
+    Port:
+        Description: The container port to bind to the ALB
+        Type: String
+
+    RoutePriority:
+        Description: The priority of Load Balancer listener rule for this service
+        Type: String
+
+    TaskDefinition:
+        Description: The ARN of the task definition for the service
+        Type: String
+
+Resources:
+
+    Service:
+        Type: AWS::ECS::Service
+        DependsOn: ListenerRule
+        Properties:
+            Cluster: !Ref Cluster
+            Role: !Ref ServiceRole
+            DesiredCount: !Ref DesiredCount
+            TaskDefinition: !Ref TaskDefinition
+            LoadBalancers:
+                - ContainerName: web
+                  ContainerPort: !Ref Port
+                  TargetGroupArn: !Ref TargetGroup
+
+    CloudWatchLogsGroup:
+        Type: AWS::Logs::LogGroup
+        Properties:
+            LogGroupName: !Ref AWS::StackName
+            RetentionInDays: 365
+
+    TargetGroup:
+        Type: AWS::ElasticLoadBalancingV2::TargetGroup
+        Properties:
+            VpcId: !Ref VPC
+            Port: 80
+            Protocol: HTTP
+            Matcher:
+                HttpCode: 200-299
+            HealthCheckIntervalSeconds: 10
+            HealthCheckPath: !Ref Path
+            HealthCheckProtocol: HTTP
+            HealthCheckTimeoutSeconds: 5
+            HealthyThresholdCount: 2
+
+    ListenerRule:
+        Type: AWS::ElasticLoadBalancingV2::ListenerRule
+        Properties:
+            ListenerArn: !Ref Listener
+            Priority: !Ref RoutePriority
+            Conditions:
+                - Field: path-pattern
+                  Values:
+                    - !Ref Path
+            Actions:
+                - TargetGroupArn: !Ref TargetGroup
+                  Type: forward
+
+    # This IAM Role grants the service access to register/unregister with the
+    # Application Load Balancer (ALB). It is based on the default documented here:
+    # http://docs.aws.amazon.com/AmazonECS/latest/developerguide/service_IAM_role.html
+    ServiceRole:
+        Type: AWS::IAM::Role
+        Properties:
+            RoleName: !Sub ecs-service-${AWS::StackName}
+            Path: /
+            AssumeRolePolicyDocument: |
+                {
+                    "Statement": [{
+                        "Effect": "Allow",
+                        "Principal": { "Service": [ "ecs.amazonaws.com" ]},
+                        "Action": [ "sts:AssumeRole" ]
+                    }]
+                }
+            Policies:
+                - PolicyName: !Sub ecs-service-${AWS::StackName}
+                  PolicyDocument:
+                    {
+                        "Version": "2012-10-17",
+                        "Statement": [{
+                                "Effect": "Allow",
+                                "Action": [
+                                    "ec2:AuthorizeSecurityGroupIngress",
+                                    "ec2:Describe*",
+                                    "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                                    "elasticloadbalancing:Describe*",
+                                    "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                                    "elasticloadbalancing:DeregisterTargets",
+                                    "elasticloadbalancing:DescribeTargetGroups",
+                                    "elasticloadbalancing:DescribeTargetHealth",
+                                    "elasticloadbalancing:RegisterTargets"
+                                ],
+                                "Resource": "*"
+                        }]
+                    }
+
+Outputs:
+
+    TargetGroup:
+        Description: Reference to the load balancer target group
+        Value: !Ref TargetGroup
+
+    ServiceRole:
+        Description: The IAM role for the service
+        Value: !Ref ServiceRole
+
+    CloudWatchLogsGroup:
+        Description: Reference to the cloudwatch logs group
+        Value: !Ref CloudWatchLogsGroup
+`))
+
+var _WebServiceCloudFormationTemplate = template.Must(template.New("webServiceCloudFormationFile").Parse(`
+Parameters:
+
+    VPC:
+        Description: The VPC that the ECS cluster is deployed to
+        Type: AWS::EC2::VPC::Id
+
     Listener:
         Description: The Application Load Balancer listener to register with
         Type: String
