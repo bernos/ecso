62ec87390b959aa199317f6b44b6e9bc1329da45
Brendan McMahon <bernos@gmail.com>
2017-07-07 14:49:50 +1000

    Add GetEcsoBucket method to EnvironmentAPI

--- pkg/ecso/api/environmentapi.go
+++ pkg/ecso/api/environmentapi.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/bernos/ecso/pkg/ecso"
 	"github.com/bernos/ecso/pkg/ecso/awsregistry"
 	"github.com/bernos/ecso/pkg/ecso/helpers"
@@ -21,6 +22,7 @@ type EnvironmentAPI interface {
 	EnvironmentDown(p *ecso.Project, env *ecso.Environment) error
 	IsEnvironmentUp(env *ecso.Environment) (bool, error)
 	SendNotification(env *ecso.Environment, msg string) error
+	GetEcsoBucket(env *ecso.Environment) (string, error)
 	GetECSServices(env *ecso.Environment) ([]*ecs.Service, error)
 	GetECSTasks(env *ecso.Environment) ([]*ecs.Task, error)
 	GetECSContainers(env *ecso.Environment) (ContainerList, error)
@@ -46,6 +48,20 @@ type EnvironmentDescription struct {
 	ECSConsoleURL            string
 	ECSClusterBaseURL        string
 	CloudFormationOutputs    map[string]string
+}
+
+func (api *environmentAPI) GetEcsoBucket(env *ecso.Environment) (string, error) {
+	reg, err := api.registryFactory.ForRegion(env.Region)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := reg.STSAPI().GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("ecso-%s-%s", env.Region, *resp.Account), nil
 }
 
 func (api *environmentAPI) DescribeEnvironment(env *ecso.Environment) (*EnvironmentDescription, error) {
---
+++ pkg/ecso/dispatcher/dispatcher.go
@@ -0,0 +1,75 @@
+package dispatcher
+
+import (
+	"fmt"
+
+	"github.com/bernos/ecso/pkg/ecso"
+	"github.com/bernos/ecso/pkg/ecso/config"
+)
+
+type CommandFactory func(*config.Config) (ecso.Command, error)
+
+// NewDispatcher creates a default Dispatcher for a Project, with the provided Config and
+// UserPreferences
+func NewDispatcher(project *ecso.Project, cfg *config.Config, prefs *ecso.UserPreferences) Dispatcher {
+	return DispatcherFunc(func(factory CommandFactory, cOptions ecso.CommandOptions, options ...func(*DispatchOptions)) error {
+		opt := &DispatchOptions{
+			EnsureProjectExists: true,
+		}
+
+		for _, o := range options {
+			o(opt)
+		}
+
+		if opt.EnsureProjectExists && project == nil {
+			return fmt.Errorf("No ecso project file was found")
+		}
+
+		ctx := ecso.NewCommandContext(project, prefs, cfg.Version, cOptions)
+
+		cmd, err := factory(cfg)
+
+		if err != nil {
+			return err
+		}
+
+		if err := cmd.Prompt(ctx); err != nil {
+			return err
+		}
+
+		if err := cmd.Validate(ctx); err != nil {
+			return err
+		}
+
+		return cmd.Execute(ctx)
+	})
+}
+
+// Dispatcher executes an ecso Command
+type Dispatcher interface {
+	Dispatch(CommandFactory, ecso.CommandOptions, ...func(*DispatchOptions)) error
+}
+
+// DispatcherFunc is an adaptor to allow the use of ordinary functions as
+// an ecso Dispatcher
+type DispatcherFunc func(CommandFactory, ecso.CommandOptions, ...func(*DispatchOptions)) error
+
+// Dispatch calls fn(cmd, options...)
+func (fn DispatcherFunc) Dispatch(factory CommandFactory, cOptions ecso.CommandOptions, dOptions ...func(*DispatchOptions)) error {
+	return fn(factory, cOptions, dOptions...)
+}
+
+// DispatchOptions alter how a Dispatcher dispatches Commands
+type DispatchOptions struct {
+	// EnsureProjectExists determines whether the dispatcher will return an
+	// error if the Project it is dispatching the Command on is nil
+	EnsureProjectExists bool
+}
+
+// SkipEnsureProjectExists is an option function that will permit dispatching
+// a Command on a Project that is nil
+func SkipEnsureProjectExists() func(*DispatchOptions) {
+	return func(opt *DispatchOptions) {
+		opt.EnsureProjectExists = false
+	}
+}
